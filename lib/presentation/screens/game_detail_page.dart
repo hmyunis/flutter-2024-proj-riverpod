e9fd55c8ce7c6006a05bc908a1eac7bfa630ccd0
EstifanosTaye320 <149786367+EstifanosTaye320@users.noreply.github.com>
2024-04-16 20:34:29 +0300

    Update game_detail_page.dart adding the buttons to the details screen

--- lib/presentation/screens/game_detail_page.dart
+++ lib/presentation/screens/game_detail_page.dart
@@ -126,7 +126,50 @@ class _GameDetailPageState extends State<GameDetailPage> {
                 ],
               ),
               const SizedBox(
-                height: 30,
+                height: 15,
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: const Text('Edit game detail'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: const Text('Delete game'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 15,
               ),
               Column(
                 children: [

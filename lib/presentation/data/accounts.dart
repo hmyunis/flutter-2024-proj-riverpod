4bc3eb7edbdb84429b82aaf05a9896e3521d15a8
EstifanosTaye320 <149786367+EstifanosTaye320@users.noreply.github.com>
2024-04-16 20:21:03 +0300

    Create accounts.dart

---
+++ lib/presentation/data/accounts.dart
@@ -0,0 +1,8 @@
+import 'package:video_game_catalogue_app/presentation/models/account.dart';
+
+List<Account> accounts = const [
+  Account(userName: "Estifanos", userType: "Admin"),
+  Account(userName: "Hamdi", userType: "User"),
+  Account(userName: "Bela", userType: "Owner"),
+  Account(userName: "Bela", userType: "Admin"),
+];

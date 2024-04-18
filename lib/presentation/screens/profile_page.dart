8354579c1f5b7e8ef12ecf0bf1244c5b92050a8d
ldmoti <saltyanalysis89@gmail.com>
2024-04-18 21:52:20 +0300

    added update for profile

--- lib/presentation/screens/profile_page.dart
+++ lib/presentation/screens/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_game_catalogue_app/presentation/data/accounts.dart';
 import 'package:video_game_catalogue_app/presentation/widgets/administrators.dart';
+
 import '../data/avatars.dart';
 import '../widgets/avatar_picker_dialog.dart';
 
@@ -14,8 +15,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController _textController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
-  int avatarIndex =
-      0; //globally set/stored later, so that it doesn't reset on every build
+  int avatarIndex = 0;
 
   @override
   void initState() {
@@ -81,9 +81,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
+                  const SizedBox(height: 10),
                   const Text(
                     'Meowsalot',
                     style: TextStyle(
@@ -92,9 +90,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+                  const SizedBox(height: 30),
                   Column(
                     children: [
                       const Row(
@@ -113,59 +109,55 @@ class _ProfilePageState extends State<ProfilePage> {
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 10),
                       TextField(
                         controller: _textController,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
+                        style: const TextStyle(color: Colors.grey),
                         decoration: InputDecoration(
                           labelText: 'Username',
                           labelStyle: const TextStyle(color: Colors.grey),
-                          prefixIcon: const Icon(
-                            Icons.person,
-                          ),
+                          prefixIcon: const Icon(Icons.person),
                           prefixIconColor: Colors.blue[700],
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 10),
                       TextField(
                         controller: _emailController,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
+                        style: const TextStyle(color: Colors.grey),
                         decoration: InputDecoration(
                           labelText: 'Email',
                           labelStyle: const TextStyle(color: Colors.grey),
-                          prefixIcon: const Icon(
-                            Icons.email,
-                          ),
+                          prefixIcon: const Icon(Icons.email),
                           prefixIconColor: Colors.blue[700],
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 10),
                       ButtonBar(
                         alignment: MainAxisAlignment.center,
                         children: [
                           ElevatedButton(
                             onPressed: () {},
                             style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.red[700]),
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.red[700]),
                             ),
                             child: const Text('Log out'),
                           ),
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              // Show bottom sheet for editing profile
+                              showModalBottomSheet(
+                                context: context,
+                                backgroundColor: Colors.blueGrey[800],
+                                builder: (context) => EditProfileBottomSheet(
+                                  initialUsername: _textController.text,
+                                  initialEmail: _emailController.text,
+                                ),
+                              );
+                            },
                             style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.blue[700]),
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.blue[700]),
                             ),
                             child: const Text('Update'),
                           ),
@@ -180,6 +172,87 @@ class _ProfilePageState extends State<ProfilePage> {
           (accounts[0].userType == "Owner" || accounts[0].userType == "Admin")
               ? const AdminContainer()
               : const SizedBox(),
+        ],
+      ),
+    );
+  }
+}
+class EditProfileBottomSheet extends StatefulWidget {
+  final String initialUsername;
+  final String initialEmail;
+
+  const EditProfileBottomSheet({super.key, 
+    required this.initialUsername,
+    required this.initialEmail,
+  });
+
+  @override
+  State<EditProfileBottomSheet> createState() => _EditProfileBottomSheetState();
+}
+
+class _EditProfileBottomSheetState extends State<EditProfileBottomSheet> {
+  late TextEditingController _usernameController;
+  late TextEditingController _emailController;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController(text: widget.initialUsername);
+    _emailController = TextEditingController(text: widget.initialEmail);
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[800],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+      ),
+      child: Column(
+        children: [
+          const Text('Edit Profile', style: TextStyle(fontSize: 20, color: Colors.white)),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _usernameController,
+            decoration: const InputDecoration(
+              labelText: 'Username',
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+          TextField(
+            controller: _emailController,
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Save the edited profile details
+              String updatedUsername = _usernameController.text;
+              String updatedEmail = _emailController.text;
+             
+              Navigator.pop(context);
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+            ),
+            child: const Text('Save Changes', style: TextStyle(color: Colors.white)),
+          ),
         ],
       ),
     );
